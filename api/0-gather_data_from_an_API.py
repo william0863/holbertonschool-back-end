4340343a9c8ac128805734fa07a556a61827ffaf
william0863 <granger.w35@gmail.com>
2023-01-07 22:21:45 +0100

    task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,31 +4,39 @@ Write a Python script that, using this REST API,
 for a given employee ID, returns information about his/her TODO list progress.
 """
 
+from requests import get
+from sys import argv
+
+
+def api_todo():
+    """
+    This function accepts an integer as a parameter, which is the employee ID
+    """
+    employee_id = int(argv[1])
+    employee_name = ""
+    number_of_task = 0
+    number_of_done_task = 0
+    titles_of_task = []
+    user_url = get("https://jsonplaceholder.typicode.com/users").json()
+    task_url = get("https://jsonplaceholder.typicode.com/todos").json()
+
+    for user in user_url:
+        if user['id'] == employee_id:
+            employee_name = user['name']
+
+    for task in task_url:
+        if task['userId'] == employee_id:
+            if task['completed']:
+                titles_of_task.append(task['title'])
+                number_of_done_task += 1
+            number_of_task += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                          number_of_done_task,
+                                                          number_of_task))
+
+    for title in titles_of_task:
+        print("\t " + title)
 
 if __name__ == "__main__":
-
-    import requests
-    from sys import argv
-    if len(argv) < 2:
-        exit()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
-        .format(argv[1]))
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}"
-        .format(argv[1]))
-    name = name.json()
-    name = name[0]["name"]
-    todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    todo = todo.json()
-    todo = len(todo)
-    todos = todos.json()
-    todo_list = []
-
-    for x in todos:
-        todo_list.append("\t {}".format(x["title"]))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(todos), todo))
-    for y in todo_list:
-        print(y)
+    api_todo()
