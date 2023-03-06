2b776e10a879578d2f3a8ae59b482a2773fbf026
william0863 <granger.w35@gmail.com>
2023-03-06 22:23:36 +0100

    update task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,35 +8,34 @@ from requests import get
 from sys import argv
 
 
-def api_todo():
-    """
-    This function accepts an integer as a parameter, which is the employee ID
-    """
-    employee_id = int(argv[1])
-    employee_name = ""
-    number_of_task = 0
-    number_of_done_task = 0
-    titles_of_task = []
-    user_url = get("https://jsonplaceholder.typicode.com/users").json()
-    task_url = get("https://jsonplaceholder.typicode.com/todos").json()
+def get_api():
+    """Collect data from API"""
+    emp_id = int(argv[1])
+    emp_name = ''
+    tasks_done = 0
+    tasks_total = 0
+    tasks_titles = []
 
-    for user in user_url:
-        if user['id'] == employee_id:
-            employee_name = user['name']
+    users_res = get('https://jsonplaceholder.typicode.com/users').json()
+    for user in users_res:
+        if user['id'] == emp_id:
+            emp_name = user['name']
+            break
 
-    for task in task_url:
-        if task['userId'] == employee_id:
+    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
+    for task in tasks_res:
+        if task['userId'] == emp_id:
             if task['completed']:
-                titles_of_task.append(task['title'])
-                number_of_done_task += 1
-            number_of_task += 1
+                tasks_titles.append(task['title'])
+                tasks_done += 1
+            tasks_total += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          number_of_done_task,
-                                                          number_of_task))
+    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
+                                                          tasks_done,
+                                                          tasks_total))
+    for title in tasks_titles:
+        print('\t {}'.format(title))
 
-    for title in titles_of_task:
-        print("\t " + title)
 
-if __name__ == "__main__":
-    api_todo()
+if __name__ == '__main__':
+    get_api()
