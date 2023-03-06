f5791fa7040122030e2707cf70184a343405841f
william0863 <granger.w35@gmail.com>
2023-03-06 22:32:55 +0100

    update task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,38 +4,32 @@ Write a Python script that, using this REST API,
 for a given employee ID, returns information about his/her TODO list progress.
 """
 
-from requests import get
-from sys import argv
 
+def counter(completed=None):
 
-def get_api():
-    """Collect data from API"""
-    emp_id = int(argv[1])
-    emp_name = ''
-    tasks_done = 0
-    tasks_total = 0
-    tasks_titles = []
+    ct = 0
+    for arg in todo:
+        if arg.get('completed') is True:
+            ct += 1
+    return ct
 
-    users_res = get('https://jsonplaceholder.typicode.com/users').json()
-    for user in users_res:
-        if user['id'] == emp_id:
-            emp_name = user['name']
-            break
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
-    for task in tasks_res:
-        if task['userId'] == emp_id:
-            if task['completed']:
-                tasks_titles.append(task['title'])
-                tasks_done += 1
-            tasks_total += 1
+    payload = {'id': argv[1]}
+    user = requests.get('https://jsonplaceholder.typicode.com/users',
+                        params=payload).json()
 
-    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
-                                                          tasks_done,
-                                                          tasks_total))
-    for title in tasks_titles:
-        print('\t {}'.format(title))
+    payload2 = {'userId': argv[1]}
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
+                        params=payload2).json()
 
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user[0].get('name'),
+        counter(todo),
+        len(todo)))
 
-if __name__ == '__main__':
-    get_api()
+    for arg in todo:
+        if arg.get('completed') is True:
+            print("\t {}".format(arg.get('title')))
